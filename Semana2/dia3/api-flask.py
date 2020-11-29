c4c931553973bf69d344e0fb7e8b9274414ef755
JuanCaychoPaucar <jc_caycho_p@outlook.com>
2020-11-29 13:18:41 -0500

    Flask, flask-restful

--- Semana2/dia3/api-flask.py
+++ Semana2/dia3/api-flask.py
@@ -17,11 +17,62 @@ def ingresar_supermercado():
     print(request.method)
     if request.method == 'GET':
         return {
-            'respuesta': supermercados
+            'ok': True,
+            'content': supermercados,
+            'message': None,
         }
     elif request.method == 'POST':
-        return "Me hiciste un POST"
+        # metodo get_json() convierte lo que me llega por el body, a un diccionario en Python
+        print(request.get_json())
+        informacion = request.get_json()
+        supermercados.append(informacion)
+
+        # le colocamos un codigo de estado, en este caso el 201 (CREATED)
+        # NOTA: Sino pasamos ningun estado, por defecto es el 200
+        # https://developer.mozilla.org/es/docs/Web/HTTP/Status
+        return {
+            'ok': True,
+            'content': supermercados,
+            'message': "Se agregó exitosamente el supermercado",
+        }, 201
     return "Se registró el supermercado"
+
+
+@app.route('/supermercado/<int:id_super>', methods=['GET', 'PUT', 'DELETE'])
+def supermercadoPorId(id_super):
+    if len(supermercados) > id_super:
+        # como verifico que el id que me mande exista en mi lista
+        if request.method == 'GET':
+            return {
+                'ok': True,
+                'content': supermercados[id_super],
+                'message': None,
+            }
+        elif request.method == 'PUT':
+            data = request.get_json()
+            supermercados[id_super] = data
+            return {
+                'ok': True,
+                'content': supermercados[id_super],
+                'message': 'Se actualizó el supermercado exitosamente',
+            }, 201
+        elif request.method == 'DELETE':
+            # pop me permite guardar el objeto eliminado, si lo requerimos
+            supermercados.pop(id_super)
+            return {
+                'ok': True,
+                'content': None,
+                'message': 'Registro eliminado',
+            }, 204
+
+    else:
+        return{
+            'ok': False,
+            'content': None,
+            'message': 'El supermercado con posicion {} no existe'.format(id_super),
+        }
+
+    return {'id': id_super}
 
 
 if __name__ == '__main__':
---
+++ Semana2/dia4/01-flaskRestful.py
@@ -0,0 +1,140 @@
+# pip install flask-restful
+
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+items = [
+    {
+        'prodNom': 'Sapolio',
+        'prodPrec': 5.40,
+        'prodCar': ['LIMPIA TODO', 'DESENGRASANTE', 'LAVAVAJILLAS']
+    },
+    {
+        'prodNom': 'Ayudin',
+        'prodPrec': 3.8,
+        'prodCar': ['ESPONJA GRATIS', 'LIQUIDO', 'DESENGRASANTE', 'PULIDOR']
+    },
+    {
+        'prodNom': 'Pepsi 3L',
+        'prodPrec': 5.8,
+        'prodCar': ['GASEOSA', 'CARBONATADA', 'LIQUIDO', 'ALTO EN AZUCAR']
+    }
+]
+
+# tener un CRUD y que ademas de acuerdo a un buscador, que me indique todos los productos que tengan esa caracteristica
+# 127.0.0.1/buscar {"palabra": "DESENGRASANTE"}
+
+
+@app.route('/')
+def inicio():
+    return 'La api funciona'
+
+
+@app.route('/buscar', methods=['POST'])
+def buscar():
+    data = request.get_json()
+    palabra = data['palabra'].lower()
+    # print(palabra)
+    resultado = []
+    for item in items:
+        # print(item['prodCar'])
+        for caracteristica in item['prodCar']:
+            if caracteristica.lower() == palabra:
+                resultado.append(item)
+    # print(caracteristica)
+    return {
+        'ok': True,
+        'content': resultado
+    }
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'prodNom',
+        type=str,
+        required=True,
+        help='Falta el nombre del producto'
+    )
+
+    parser.add_argument(
+        'prodPrec',
+        type=float,
+        required=True,
+        help='Falta el precio del producto'
+    )
+
+    parser.add_argument(
+        'prodCar',
+        type=list,
+        required=False,
+        location='json',
+        help='Falta las caracteristicas del producto'
+    )
+
+    def get(self, id):
+        if len(items) > id:
+            return {
+                'ok': True,
+                'content': None,
+                'message': items[id],
+            }
+        else:
+            return {
+                'ok': False,
+                'content': 'No se encontro el item a buscar',
+                'message': None,
+            }
+
+    def post(self):
+        data = self.parser.parse_args()
+        items.append(data)
+        return {
+            'ok': True,
+            'content': data,
+            'message': 'Se agrego exitosamente el item',
+        }
+
+    def put(self, id):
+        if len(items) > id:
+            data = self.parser.parse_args()
+            items[id] = data
+            return {
+                'ok': True,
+                'content': data,
+                'message': 'Se actualizo el item',
+            }, 201
+        else:
+            return {
+                'ok': False,
+                'content': None,
+                'message': 'No se encontro el item a actualizar',
+            }
+
+    def delete(self, id):
+        if len(items) > id:
+            items.pop(id)
+            return {
+                'ok': True,
+                'content': None,
+                'message': 'Se elimino el item',
+            }
+        else:
+            return {
+                'ok': False,
+                'content': None,
+                'message': 'No se encontro el item a actualizar',
+            }
+
+
+# Con el uso de flask_restfull ya no se necesitan decoradores.
+# Solamente un parametro para agregar un recurso a la api
+
+
+api.add_resource(Item, '/item', '/item/<int:id>')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5002)
