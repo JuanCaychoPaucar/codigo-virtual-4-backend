1372a49e4201a8b1183624463ad8e6d80171a59a
JuanCaychoPaucar <jc_caycho_p@outlook.com>
2021-01-04 19:36:55 -0500

    Primeros pasos con express

---
+++ Semana6/Tareas/src/config/Server.js
@@ -0,0 +1,38 @@
+// npm install express
+const express = require('express');
+
+// npm install body-parser
+const bodyParser = require('body-parser');
+
+class Server {
+    constructor() {
+        // app es una instancia de mi clase express
+        this.app = express();
+        // en produccion nos asigan un puerto. En nuesta PC usaremos el puerto 5000
+        this.puerto = process.env.PORT || 5000;
+        this.configurarBodyParser();
+        this.rutas();
+    }
+
+    // bodyparser es la forma en la cual el front me va a mandar informacion, y yo tengo que definir que tipo de informacion voy a recibir (texto planp, json, xml ...)
+    configurarBodyParser() {
+        this.app.use(bodyParser.json());
+    }
+
+    // aca voy a definir todas mis rutas raices de mi aplicacion, para que cuando sean consultadas, si no estan aqui automaticamente express retornara un 404 (not found)
+    rutas() {
+        // primero pongo la ruta (endpoint) y luego su comportamiento (que va as hacer cuando se llame a esa ruta)
+        this.app.get('/', (req, res) => {
+            // toda la logica de esa ruta
+            res.status(200).send('La API funciona con éxito')
+        })
+    }
+
+    start() {
+        this.app.listen(this.puerto, () => {
+            console.log('El servidor esta corriendo exitosamente en el puerto ', this.puerto);
+        })
+    }
+}
+
+module.exports = Server
---
+++ Semana6/Tareas/src/index.js
@@ -0,0 +1,6 @@
+const Server = require('./config/Server');
+
+// creo una instancia de mi clase importada
+const miServidor = new Server();
+miServidor.start();
+// http://localhost:5000/
